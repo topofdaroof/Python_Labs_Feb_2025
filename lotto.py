2fb0516d1a08bb5a02658a11c4ff04ace600ff20
Donald Cameron <donald.cameron@ntlworld.com>
2024-11-19 19:06:13 +0000

    Updated lotto.py

--- lotto.py
+++ lotto.py
@@ -6,5 +6,15 @@
 """
     Docstring
 """
+import random
+lotto = [] # Create an empty list.
 
-print()
+while len(lotto) < 6:
+    num = random.randint(1, 50)
+    if num not in lotto:
+        lotto.append(num)
+    else:
+        print("Duplicate number:", num)
+
+
+print("Lottery numbers =", lotto)
