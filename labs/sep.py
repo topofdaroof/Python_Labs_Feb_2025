162d7517c6887a9aa206f2dc56b9e54689f6cd23
Donald Cameron <donald.cameron@ntlworld.com>
2024-11-19 23:33:40 +0000

    Modified sep.py

--- labs/sep.py
+++ labs/sep.py
@@ -1,4 +1,13 @@
 #! /usr/bin/python
-
+#                    1         2         3         4
+#          012345678901234567890123456789012345678901234567890
 Belgium = 'Belgium,10445852,Brussels,737966,Europe,1830,Euro,Catholicism,Dutch,French,German'
 
+print("-" * len(Belgium))
+print(Belgium.replace(",", ":"))
+
+pop_country = int(Belgium[8:16])
+pop_capital = int(Belgium[26:32])
+
+print(f"Population of Country and capital = {pop_country + pop_capital}")
+print("-" * len(Belgium))
