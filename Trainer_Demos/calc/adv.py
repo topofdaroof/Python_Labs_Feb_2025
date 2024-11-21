bfc6cd8a54b6c81a7d5c9f962dbd98b254ad5a82
Donald Cameron <donald.cameron@ntlworld.com>
2024-11-21 21:38:17 +0000

    Updated basic and adv with namespace trick

--- Trainer_Demos/calc/adv.py
+++ Trainer_Demos/calc/adv.py
@@ -4,6 +4,7 @@
 """
     Advanced Calculator app with power, modulus and sqrt functions
 """
+import sys
 
 def power(x, z):
     """ Return the power of x to z as a float """
@@ -17,7 +18,16 @@ def sqrt(x):
     """ Return the Square root of x as a float """
     return round(x**0.5, 3)
 
-print("##### Advanced Calc App #####")
-print(f"5 ** 2 = {power(5, 2)}")
-print(f"5 % 2 = {mod(5, 2)}")
-print(f"\N{square root}5 = {sqrt(5)}")
+def main():
+    print("##### Advanced Calc App #####")
+    print(f"5 ** 2 = {power(5, 2)}")
+    print(f"5 % 2 = {mod(5, 2)}")
+    print(f"\N{square root}5 = {sqrt(5)}")
+    return None
+
+# Namespace Trick
+if __name__ == "__main__":
+    # EXECUTE only if ran directly as a program.
+    # Ignore if imported.
+    main()
+    sys.exit(0)
--- Trainer_Demos/calc/basic.py
+++ Trainer_Demos/calc/basic.py
@@ -5,6 +5,10 @@
     Basic Calculator App with add, multiply, subtract
     and divide functions.
 """
+import sys
+import adv # SAFER
+# from adv import * # Beware of namespace pollution
+# from adv import power
 
 def add(*args):
     """ Return SUM of all parameters as a float """
@@ -28,10 +32,21 @@ def div(x, z):
     """ Return Quotient of x divided by z to 3 decimal places """
     return round(x/z, 3)
 
-print("***** BASIC Calculator APP *****")
-print(f"4 + 3 = {add(4, 3)}")
-print(f"4 + 3 + 2 + 1 = {add(4, 3, 2, 1)}")
-print(f"4 * 3 = {mul(4, 3)}")
-print(f"4 * 3  * 2 = {mul(4, 3, 2)}")
-print(f"4 - 3 = {sub(4, 3)}")
-print(f"4 / 3 = {div(4, 3)}")
+def main():
+    print("***** BASIC Calculator APP *****")
+    print(f"4 + 3 = {add(4, 3)}")
+    print(f"4 + 3 + 2 + 1 = {add(4, 3, 2, 1)}")
+    print(f"4 * 3 = {mul(4, 3)}")
+    print(f"4 * 3  * 2 = {mul(4, 3, 2)}")
+    print(f"4 - 3 = {sub(4, 3)}")
+    print(f"4 / 3 = {div(4, 3)}")
+
+    print(f"Advanced Mode -> 5 to the power 4 = {adv.power(5, 4)}")
+    return None
+
+# Namespace Trick
+if __name__ == "__main__":
+    # EXECUTE only if ran directly as a program.
+    # Ignore if imported.
+    main()
+    sys.exit(0)
