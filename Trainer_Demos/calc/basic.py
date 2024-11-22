348b509362f7f91f76825ec47f51c8a576919366
Donald Cameron <donald.cameron@ntlworld.com>
2024-11-22 01:17:26 +0000

    Created TestCases and Test Runners

--- Trainer_Demos/calc/basic.py
+++ Trainer_Demos/calc/basic.py
@@ -11,25 +11,39 @@ import adv # SAFER
 # from adv import power
 
 def add(*args):
-    """ Return SUM of all parameters as a float """
+    """ Return SUM of all parameters as a float
+    >>> add(4, 3, 2)
+    9.0
+    """
     total = 0
     for num in args:
         total += num
     return float(total)
 
 def sub(x, z):
-    """ Return result of x subtract x as a float """
+    """ Return result of x subtract x as a float
+    >>> sub(4, 3)
+    1.0
+    """
     return float(x-z)
 
 def mul(*args):
-    """ Return PRODUCT of all parameters as a float """
+    """ Return PRODUCT of all parameters as a float
+    >>> mul(4, 3)
+    12.0
+    >>> mul(4, 3, 2)
+    24.0
+    """
     total = 1
     for num in args:
         total *= num
     return float(total)
 
 def div(x, z):
-    """ Return Quotient of x divided by z to 3 decimal places """
+    """ Return Quotient of x divided by z to 3 decimal places
+    >>> div(4, 3)
+    1.333
+    """
     return round(x/z, 3)
 
 def main():
@@ -48,5 +62,7 @@ def main():
 if __name__ == "__main__":
     # EXECUTE only if ran directly as a program.
     # Ignore if imported.
+    import doctest
+    doctest.testmod() # Scan through Docstrings and automated Tests!
     main()
     sys.exit(0)
