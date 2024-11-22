ca317976dfa1d9e998ebf3cb3dcf6b14a24249aa
Donald Cameron <donald.cameron@ntlworld.com>
2024-11-22 00:27:03 +0000

    Updated user function and added a try/except/else/finally block for Exception Handling

--- Trainer_Demos/demo_regex_user_function.py
+++ Trainer_Demos/demo_regex_user_function.py
@@ -6,21 +6,43 @@
     Docstring:
 """
 import re
+import sys
 
 # Example of a user function with parameter passing and default values.
 def search_pattern(pattern=r"^.{28}$", file=r"c:\labs\projects\Python3_labs\labs\words"):
-    fh_in = open(file, mode="rt")
-    lines = 0
-    for line in fh_in:
-        m = re.search(pattern, line)  # Search for pattern in each line of file.
-        if m:
-            lines += 1
-            print(f"Matched {m.group()} on {line.rstrip()} at {m.start()} - {m.end()}")
+    try:
+        fh_in = open(file, mode="rt")
+    except PermissionError as err:
+        print(f"Error {err.args[0]}, reason={err.args[1]}, {err.filename}", file=sys.stderr)
+        sys.exit(1)
+    except FileNotFoundError as err:
+        print(f"Error {err.args[0]}, reason={err.args[1]}, {err.filename}", file=sys.stderr)
+        sys.exit(2)
+    except BaseException as err:
+        print(f"Error occurred, investigate", file=sys.stderr)
+        sys.exit(3)
+    else:
+        # Executes if try block is successful..
+        lines = 0
+        for line in fh_in:
+            m = re.search(pattern, line)  # Search for pattern in each line of file.
+            if m:
+                lines += 1
+                print(f"Matched {m.group()} on {line.rstrip()} at {m.start()} - {m.end()}")
 
-    fh_in.close() # Flush buffers and close file handle.
+        fh_in.close() # Flush buffers and close file handle.
+    finally:
+        # Always executes if try block fails/succeeds
+        print("And now for something completely different..")
     return lines
 
+def main():
+    search_pattern(r"^.{19}$", r"c:\labs\projects\Python3_labs\labs\wrds")
+    # num_lines = search_pattern()
+    # print(f"Matched {num_lines}")
+    return None
 
-search_pattern(r"^.{19}$", r"c:\labs\projects\Python3_labs\labs\words")
-num_lines = search_pattern()
-print(f"Matched {num_lines}")
+# Namespace Trick
+if __name__ == "__main__":
+    main()
+    sys.exit(0)
