204a535f16729f6a7d695d30f5e72d4fd9dc713d
Donald Cameron <donald.cameron@ntlworld.com>
2025-02-03 22:52:48 +0000

    Added first.py

---
+++ first.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+# Author: DCameron
+# Version: 1.0
+# Description: This is our first script
+
+# name = "Donald Cameron"
+name = input("Enter your name: ")
+
+print("My name is", name)
+print("My name is " + name)
+
+import random
+print("Lucky number is", random.randint(1, 50))
+
+import math
+print("Cosine of 0.5 is", math.cos(0.5))
