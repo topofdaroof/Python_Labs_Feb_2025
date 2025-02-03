54cdced49d5f90ebad4469bdab7e52e0a76b78bb
Euler Cruz <eulwrites2@gmail.com>
2025-02-03 22:57:43 +0000

    added first-exercise-script.py

---
+++ first-exercise-script.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+# Author: Euls
+# Version: 1.0
+# Description: This is our first script
+
+# name = "Dale Gribble"
+name = input("Enter your name: ")
+
+print("My name is", name)
+print("My name is " + name)
+
+import random
+print("Lucky number is", random.randint(1,50))
+
+import math
+print("Cosine of 0.5 is", math.cos(0.5))
