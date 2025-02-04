93c52c152255422f4db874396560652cfe6cb0e2
Euler Cruz <eulwrites2@gmail.com>
2025-02-04 20:37:20 +0000

    Added er-demo_counter-loops.py

---
+++ er-demo_iterator_loop.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+# Author: Eul
+# Description: This script will demo HOWTO ITERATE through a SEQUENCE
+"""
+Docstring:
+"""
+
+films = ['hust and flow', 'B film' , 'lord of the rings' , 'c movie' , 'd film' , 'e movie', 'f film' , 'g movie']
+
+for kubricks in films:
+    print(kubricks.upper (), end = "\n")
+
+idx = 0
