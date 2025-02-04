338b32ab776ff6b65c0df2b167988f7305a688f0
Euler Cruz <eulwrites2@gmail.com>
2025-02-04 19:37:46 +0000

    Added er-demo_pin.py

---
+++ er-demo_pin.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+# Author: Eul
+# Description: This script will simulate a high street bank
+"""
+Docstring:
+"""
+
+
+master_pin = "0123"
+pin = None
+attempts = 0
+
+# Loop whilst PIN is incorrect
+
+while pin != master_pin and attempts < 3:
+    pin = input("Enter Pin: ")
+    if pin == master_pin:
+        print("Valid Pin")
+    else:
+        print("Invalid Pin")
+        attempts += 1
+
+print("done")
