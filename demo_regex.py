62e4beacfbce8f26b34621ee637a9a9912f79a7c
Donald Cameron <donald.cameron@ntlworld.com>
2025-02-05 19:17:29 +0000

    Updated demo_regex

--- demo_regex.py
+++ demo_regex.py
@@ -13,8 +13,24 @@ fh_in = open(r"c:\labs\words", mode="rt")
 for line in fh_in:
     # Example of str testing = GOOD!
     # if line.startswith("Y") and line.rstrip("\n").endswith("n") and "town" in line:
-    # m = re.search("^the", line) # Match lines starting with 'the'
-    # m = re.search("ing$", line)  # Match lines ending with 'ing'
-    m = re.search("^ring$", line)  # Match lines ending with 'ing'
+    # m = re.search(r"^the", line) # Match lines starting with 'the'
+    # m = re.search(r"ing$", line)  # Match lines ending with 'ing'
+    # m = re.search(r"^ring$", line)  # Match lines ending with 'ing'
+    # m = re.search(r"^.ing$", line)  # Match lines of 4 chars ending with 'ing'
+    # m = re.search(r"^...................$", line)  # Match lines of exactly 19 chars
+    # m = re.search(r"^.{19}$", line)  # Match lines of exactly 19 chars
+    # m = re.search(r"^[adr]ing$", line)  # Match lines with [adr] followed 'ing'
+    # m = re.search(r"^[A-Z]", line)  # Match lines starting with a CAPITAL
+    # m = re.search(r"[aeiou][aeiou][aeiou]", line)  # Match lines 3 consecutive vowels
+    # m = re.search(r"[aeiou]{5,}", line)  # Match lines 5 consecutive vowels
+    # m = re.search(r"[0-9][0-9]", line)  # Match lines 2 consecutive digits
+    # m = re.search(r"\.", line)  # Match lines with a DOT. You will be WARNED!
+    # m = re.search(r"[.]", line)  # ANOTHER WAY TO ESCAPE!
+    # m = re.search(r"^[A-Z].*[A-Z]$", line)  # Match lines start/end with a CAPITAL
+    # m = re.search(r"^[A-Z].{4}[A-Z]$", line)  # Match lines of 6 chars start/end with a CAPITAL
+    # m = re.match(r"^[A-Z].{4}[A-Z]$", line)  # AUTO MATCHES LINES STARTING WITH...
+    # m = re.fullmatch(r"^[A-Z].{4}[A-Z]\n$", line)  # Match ENTIRE LINE incl HIDDEN chars!
+    # m = re.search(r"^(.)(.).\2\1$", line)  # Match lines 5 char palindromes
+    m = re.search(r"^([A-Z]).*\1$", line)  # Match lines start/end with SAME CAPITAL!
     if m:
         print(line, end="")
