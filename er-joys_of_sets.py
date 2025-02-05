44f43e3eeab6bd46601c9b4d78670f04fed933c9
Euler Cruz <eulwrites2@gmail.com>
2025-02-05 01:25:23 +0000

    Added exercise files from Python course

---
+++ er-joys_of_sets.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+# Author: Eul
+# Description: This script will demo
+"""
+Docstring:
+"""
+
+marvel_fans = {'donald', 'bob', 'aram', 'rick', 'morty'}
+dc_fans = set() # create an empty SET
+
+# Grow a SET
+dc_fans.add('mike')
+dc_fans.add('zane')
+dc_fans.add('will')
+
+print(f"something something ")
