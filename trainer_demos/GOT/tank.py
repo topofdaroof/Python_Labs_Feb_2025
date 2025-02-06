5636599a67ba3590d395116f0a8f563ef202cefa
Donald Cameron <donald.cameron@ntlworld.com>
2025-02-06 22:57:20 +0000

    Added Vehicle Base Class Module

--- trainer_demos/GOT/tank.py
+++ trainer_demos/GOT/tank.py
@@ -8,22 +8,12 @@
 class Tank:
     # Attributes/Data + Behaviour/Methods
     def __init__(self, country, model):
-        self.country = country
-        self.model = model
-        self._speed = 0
+
         self._location = {'x':0, 'y':0, 'z':0}
         self._direction = 0
         self._shells = 20
         self._health = 100
         # No Explicit Return, as called implicitly.
-
-    def accel(self, increase):
-        self._speed += increase
-        return None
-
-    def decel(self, decrease):
-        self._speed -= decrease
-        return None
 
     def rotate_left(self, degrees):
         self._direction -= degrees % 360
