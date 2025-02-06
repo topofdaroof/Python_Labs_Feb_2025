3e43b5ab87ede07ba358808abfb52137dccdd221
Euler R <eulwrites2@gmail.com>
2025-02-06 19:39:19 +0000

    pushing out - day 4 class - Advanced Calculator! after namespace trick

--- er-training demos/er-calc/er-calc.py
+++ er-training demos/er-calc/er-calc.py
@@ -19,10 +19,10 @@ menu = """"
 """
 print(menu)
 option = input("Choose 1 or 2: ")
-if option == 1:
+if option == "1":
     print(f"100 + 50 + 25 + 12.5 = {add(100, 50, 25, 12.5)}")
     print(f"100 * 50 * 25 * 12.5 = {mul(100, 50, 25, 12.5)}")
-elif option == 2:
+elif option == "2":
     print(f"Modulus of 100 by 45 = {mod(100, 45)}")
     print(f"square root of 25 = {sqrt(25)}")
 else:
