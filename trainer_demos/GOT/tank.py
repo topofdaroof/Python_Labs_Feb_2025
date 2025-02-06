412f0e6832e2ea28184c43230de61ce5dfd0fa21
Donald Cameron <donald.cameron@ntlworld.com>
2025-02-06 21:58:32 +0000

    Added tank module

---
+++ trainer_demos/GOT/tank.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python3
+# Author: DCameron
+# Description: This module describes a Class of Tank
+"""
+    Tank class for - Game of Tanks
+"""
+
+class Tank:
+    # Attributes/Data + Behaviour/Methods
+    def __init__(self, country, model):
+        self.country = country
+        self.model = model
+        self._speed = 0
+        self._location = {'x':0, 'y':0, 'z':0}
+        self._direction = 0
+        self._shells = 20
+        self._health = 100
+        # No Explicit Return, as called implicitly.
+
+    def accel(self, increase):
+        self._speed += increase
+        return None
+
+    def decel(self, decrease):
+        self._speed -= decrease
+        return None
+
+    def rotate_left(self, degrees):
+        self._direction -= degrees % 360
+        return None
+
+    def rotate_right(self, degrees):
+        self._direction += degrees % 360
+        return None
+
+    def shoot(self):
+        self._shells -= 1
+        return None
+
+    def take_damage(self, damage):
+        self._health -= damage
+        return None
